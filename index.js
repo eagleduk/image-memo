797d64d1052613f44506f22dee1e455df565ea71
SJ.Lee <eagleduk@gmail.com>
2025-12-29 22:55:07 +0900

    proxy 관련 다시 해야 할듯. 상속구조로 테스트 필요

--- index.js
+++ index.js
@@ -67,7 +67,6 @@ class ImageMemoProxy {
   _state = _DEFAULTSTATE;
   _data = null;
 
-  //Ques?? 이걸 그냥 class 에 넣으면 어떰?
   constructor() {
     return new Proxy(this, {
       set: (object, key, value, proxy) => {
@@ -76,9 +75,7 @@ class ImageMemoProxy {
           console.log("값 변경");
           object[key] = value;
 
-          if (key === "_state") {
-            this.changeState();
-          }
+          if (key === "_focus") this.changeFocus();
         }
         return true;
       },
@@ -90,37 +87,159 @@ class ImageMemoProxy {
   }
 }
 
-class ImageMemo extends ImageMemoProxy {
+class ImageMemo {
+  #rootId;
+  // #rootId;
+  #options = { TST: true };
+  #memos = [];
+  #paths = [];
+  #ids = [];
+  #focus = null;
+  #state = _DEFAULTSTATE;
+  #data = null;
+
+  #setProperty() {
+    console.log("setProperty");
+    return new Proxy(
+      {
+        "#rootId": "TEST",
+      },
+      {
+        set: (object, key, value, proxy) => {
+          console.log("set ", key, object[key], value, object[key] !== value);
+          if (object[key] !== value) {
+            console.log("값 변경");
+            object[key] = value;
+
+            if (key === "#state") {
+              this.changeState();
+            }
+
+            if (key === "#focus") {
+              console.log("포커스 변경", value);
+            }
+          }
+          return true;
+        },
+        get: (target, key) => {
+          console.log("get", target, key);
+          return target[key];
+        },
+      }
+    );
+  }
+
   constructor(rootId, opt = {}) {
-    // console.log(this._rootId, " ==== ", rootId);
-    super();
-    this._rootId = rootId;
-    this._options = {
-      ...this._options,
-      ...opt,
-    };
-    this._memos = [];
-    this._ids = [];
-    this._focus = null;
+    console.log("this", this);
+    // this.#rootId = rootId;
+    // this._rootId = rootId;
+    // this.#options = {
+    //   ...this.#options,
+    //   ...opt,
+    // };
+    // this.#memos = [];
+    // this.#ids = [];
+    // this.#focus = null;
+    // this.#data = null;
+    return new Proxy(
+      {
+        "#rootId": rootId,
+        rootId: rootId,
+        "#options": opt,
+        "#memos": [],
+        "#ids": [],
+        "#focus": null,
+        "#data": null,
+        setInfo: this.setInfo,
+        "#render": this.#render,
+        render: this.render,
+        onResetFocus: this.#onResetFocus,
+      },
+      {
+        set: (object, key, value, proxy) => {
+          console.log("set ", key, object[key], value, object[key] !== value);
+          if (object[key] !== value) {
+            console.log("값 변경");
+            object[key] = value;
+
+            if (key === "#state") {
+              this.changeState();
+            }
+
+            if (key === "#focus") {
+              console.log("포커스 변경", value);
+            }
+          }
+          return true;
+        },
+        get: (target, key) => {
+          console.log("get", target, key);
+          return target[key];
+        },
+      }
+    );
+
+    //super();
+  }
+
+  render() {
+    console.log("FJEIOFJEOI");
+  }
+
+  setInfo(rootId, opt = {}) {
+    // console.log(this.#rootId);
+    // this.#setProperty();
+
+    // this.#rootId = rootId;
+    // // this._rootId = rootId;
+    // this.#options = {
+    //   ...this.#options,
+    //   ...opt,
+    // };
+    // this.#memos = [];
+    // this.#ids = [];
+    // this.#focus = null;
+    // this.#data = null;
 
     // this.#render();
+    this.render();
 
-    // window.addEventListener("click", this.#onResetFocus);
+    console.log("#rootId", this.#rootId);
 
-    // this.addEventListener("dataChange", (event) => {
-    //   console.log(
-    //     `Property "${event.detail.property}" changed from ${event.detail.oldValue} to ${event.detail.newValue}`
-    //   );
-    // });
-
-    this.#render();
+    window.addEventListener("click", this.onResetFocus);
   }
-  changeState() {
-    console.log("changeState");
+
+  destroy() {
+    window.removeEventListener("click", this.#onResetFocus);
   }
+
+  #onResetFocus = (e) => {
+    const element = e.target;
+
+    if (this.#state === _TEXTSTATE) {
+      this.#state === _DEFAULTSTATE;
+      return;
+    }
+
+    if (this.#state !== _DEFAULTSTATE) return;
+
+    if (
+      element.nodeName === "ARTICLE" ||
+      (element.nodeName === "INPUT" && element.type === "color")
+    ) {
+      return;
+    }
+
+    this.#memos.forEach((memo) => {
+      memo.classList.remove("focus");
+    });
+
+    this.#focus = null;
+  };
+
   #render() {
-    console.log("render", this._rootId);
-    const root = document.getElementById(this._rootId);
+    console.log("render", this.#rootId);
+    const root = document.getElementById(this.#rootId);
 
     const toolbar = this.#renderToolbar();
     const content = this.#renderContent();
@@ -128,14 +247,15 @@ class ImageMemo extends ImageMemoProxy {
     root.appendChild(toolbar);
     root.appendChild(content);
   }
+
   #reset() {
-    const root = document.getElementById(this._rootId);
+    const root = document.getElementById(this.#rootId);
 
     root.querySelectorAll("article").forEach((e) => e.remove());
 
     root.querySelectorAll("path").forEach((e) => e.remove());
 
-    this._data = null;
+    this.#data = null;
   }
 
   #renderToolbar() {
@@ -145,7 +265,7 @@ class ImageMemo extends ImageMemoProxy {
     const fileEl = document.createElement("input");
     fileEl.type = "file";
     fileEl.addEventListener("change", (e) => {
-      const imgEl = document.getElementById(this._rootId + "_image");
+      const imgEl = document.getElementById(this.#rootId + "_image");
       const uploadFile = e.target.files;
       if (!uploadFile || uploadFile?.length === 0) {
         imgEl.src = "";
@@ -162,7 +282,7 @@ class ImageMemo extends ImageMemoProxy {
     const saveEl = document.createElement("button");
     saveEl.textContent = "SAVE";
     saveEl.addEventListener("click", (e) => {
-      const memos = this._memos.map((memo) => {
+      const memos = this.#memos.map((memo) => {
         const {
           innerHTML,
           style: { top, left, backgroundColor, color },
@@ -180,7 +300,7 @@ class ImageMemo extends ImageMemoProxy {
         };
       });
 
-      const lines = this._paths.map((path) => {
+      const lines = this.#paths.map((path) => {
         const {
           attributes: {
             id: { value: id },
@@ -209,17 +329,17 @@ class ImageMemo extends ImageMemoProxy {
     const addBtnEl = document.createElement("button");
     addBtnEl.textContent = "[ T ]";
     addBtnEl.addEventListener("click", (e) => {
-      this._state = _TEXTSTATE;
+      this.#state = _TEXTSTATE;
 
       // const memo = this.#createMemo();
-      // const canvasEl = document.getElementById(this._rootId + "_canvas");
+      // const canvasEl = document.getElementById(this.#rootId + "_canvas");
       // canvasEl.appendChild(memo);
     });
 
     const addLineBtnEl = document.createElement("button");
     addLineBtnEl.textContent = "[ L ]";
     addLineBtnEl.addEventListener("click", (e) => {
-      this._state = _LINESTATE;
+      this.#state = _LINESTATE;
     });
 
     toolbar.appendChild(fileEl);
@@ -244,46 +364,47 @@ class ImageMemo extends ImageMemoProxy {
 
     const canvas = document.createElement("div");
     canvas.className = "image_wrapper";
-    canvas.id = this._rootId + "_canvas";
+    canvas.id = this.#rootId + "_canvas";
 
     const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-    svg.id = this._rootId + "_paint";
+    svg.id = this.#rootId + "_paint";
     svg.setAttribute("class", "image_paint");
-    svg.setAttribute("target-anchor", "--" + this._rootId + "-image");
+    svg.setAttribute("target-anchor", "--" + this.#rootId + "-image");
 
     const image = document.createElement("img");
-    image.style.anchorName = "--" + this._rootId + "-image";
-    image.id = this._rootId + "_image";
+    image.style.anchorName = "--" + this.#rootId + "-image";
+    image.id = this.#rootId + "_image";
     image.addEventListener("load", (e) => {
-      const paint = document.getElementById(this._rootId + "_paint");
+      const paint = document.getElementById(this.#rootId + "_paint");
       paint.setAttribute("width", image.clientWidth);
       paint.setAttribute("height", image.clientHeight);
 
-      this.add(this._data);
+      this.add(this.#data);
     });
 
     svg.addEventListener("contextmenu", (e) => {
       e.preventDefault();
-      if (this._state === _DRAWINGSTATE) {
-        this._state = _DEFAULTSTATE;
-        document.getElementById(this._focus).remove();
-        this._paths.pop();
+      if (this.#state === _DRAWINGSTATE) {
+        this.#state = _DEFAULTSTATE;
+        document.getElementById(this.#focus).remove();
+        this.#paths.pop();
       }
     });
 
     svg.addEventListener("click", (e) => {
-      if (this._state === _TEXTSTATE) {
-        const paint = document.getElementById(this._rootId + "_paint");
+      if (this.#state === _TEXTSTATE) {
+        const paint = document.getElementById(this.#rootId + "_paint");
         const rect = paint.getBoundingClientRect();
-        const memo = this.#createMemo("", {
+        const memo = this.#createMemo(null, {
           x: e.pageX - rect.left,
           y: e.pageY - rect.top,
         });
-        const canvasEl = document.getElementById(this._rootId + "_canvas");
+
+        const canvasEl = document.getElementById(this.#rootId + "_canvas");
         canvasEl.appendChild(memo);
 
-        this._state = _DEFAULTSTATE;
-      } else if (this._state === _LINESTATE) {
+        this.#state = _DEFAULTSTATE;
+      } else if (this.#state === _LINESTATE) {
         const path = document.createElementNS(
           "http://www.w3.org/2000/svg",
           "path"
@@ -291,12 +412,12 @@ class ImageMemo extends ImageMemoProxy {
         const uuidStr = window.crypto
           .getRandomValues(new Uint32Array(1))[0]
           .toString(36);
-        path.setAttribute("id", this._rootId + "_path_" + uuidStr);
+        path.setAttribute("id", this.#rootId + "_path_" + uuidStr);
         path.setAttribute("fill", "none");
         path.setAttribute("stroke", "red");
         path.setAttribute("stroke-width", 3);
         path.setAttribute("stroke-linejoin", "round");
-        var pt = getMousePosition(e, this._rootId + "_paint");
+        var pt = getMousePosition(e, this.#rootId + "_paint");
 
         path.dataset.startX = pt.x;
         path.dataset.startY = pt.y;
@@ -305,18 +426,18 @@ class ImageMemo extends ImageMemoProxy {
         const strPath = "M" + pt.x + " " + pt.y;
         // path.setAttribute("d", strPath);
 
-        const canvasEl = document.getElementById(this._rootId + "_paint");
+        const canvasEl = document.getElementById(this.#rootId + "_paint");
         canvasEl.appendChild(path);
 
-        this._state = _DRAWINGSTATE;
-        this._focus = this._rootId + "_path_" + uuidStr;
-        this._paths.push(path);
-      } else if (this._state === _DRAWINGSTATE) {
-        const focus = this._focus;
+        this.#state = _DRAWINGSTATE;
+        this.#focus = this.#rootId + "_path_" + uuidStr;
+        this.#paths.push(path);
+      } else if (this.#state === _DRAWINGSTATE) {
+        const focus = this.#focus;
 
         // const endPath = document.getElementById(focus);
 
-        const { x, y } = updateSvgPath(e, this._rootId + "_paint", focus);
+        const { x, y } = updateSvgPath(e, this.#rootId + "_paint", focus);
 
         const path = document.createElementNS(
           "http://www.w3.org/2000/svg",
@@ -325,12 +446,12 @@ class ImageMemo extends ImageMemoProxy {
         const uuidStr = window.crypto
           .getRandomValues(new Uint32Array(1))[0]
           .toString(36);
-        path.setAttribute("id", this._rootId + "_path_" + uuidStr);
+        path.setAttribute("id", this.#rootId + "_path_" + uuidStr);
         path.setAttribute("fill", "none");
         path.setAttribute("stroke", "red");
         path.setAttribute("stroke-width", 3);
         path.setAttribute("stroke-linejoin", "round");
-        var pt = getMousePosition(e, this._rootId + "_paint");
+        var pt = getMousePosition(e, this.#rootId + "_paint");
 
         path.dataset.startX = pt.x;
         path.dataset.startY = pt.y;
@@ -338,21 +459,22 @@ class ImageMemo extends ImageMemoProxy {
         // appendToBuffer(pt);
         // path.setAttribute("d", strPath);
 
-        const canvasEl = document.getElementById(this._rootId + "_paint");
+        const canvasEl = document.getElementById(this.#rootId + "_paint");
         canvasEl.appendChild(path);
 
-        this._state = _DRAWINGSTATE;
-        this._focus = this._rootId + "_path_" + uuidStr;
-        this._paths.push(path);
+        this.#state = _DRAWINGSTATE;
+        this.#focus = this.#rootId + "_path_" + uuidStr;
+        this.#paths.push(path);
       }
     });
 
     svg.addEventListener("mousemove", (e) => {
-      if (this._state === _DRAWINGSTATE) {
-        const focus = this._focus;
+      console.log("mousemove", this.#state, this.#focus);
+      if (this.#state === _DRAWINGSTATE) {
+        const focus = this.#focus;
         // appendToBuffer();
-        // getMousePosition(e, this._rootId + "_paint");
-        const { x, y } = updateSvgPath(e, this._rootId + "_paint", focus);
+        // getMousePosition(e, this.#rootId + "_paint");
+        const { x, y } = updateSvgPath(e, this.#rootId + "_paint", focus);
       }
     });
 
@@ -364,20 +486,20 @@ class ImageMemo extends ImageMemoProxy {
     return content;
   }
 
-  #createMemo(text = "", { x = 0, y = 0 }) {
+  #createMemo(text = null, { x = 0, y = 0 }) {
     const uuidStr = window.crypto
       .getRandomValues(new Uint32Array(1))[0]
       .toString(36);
 
-    const memoId = this._rootId + "_memo_" + uuidStr;
+    const memoId = this.#rootId + "_memo_" + uuidStr;
 
     const memo = document.createElement("article");
     memo.draggable = true;
     memo.id = memoId;
     memo.className = "memo";
-    memo.dataset.anchor = "--" + this._rootId + "-image";
-    memo.style.top = y + "px";
-    memo.style.left = x + "px";
+    memo.dataset.anchor = "--" + this.#rootId + "-image";
+    memo.style.top = y - (text === null ? 15 : 0) + "px";
+    memo.style.left = x - (text === null ? 50 : 0) + "px";
 
     memo.addEventListener("dragstart", (e) => {
       const {
@@ -386,13 +508,18 @@ class ImageMemo extends ImageMemoProxy {
         target: { clientWidth, clientHeight },
       } = e;
 
+      console.log(clientWidth, clientHeight, layerX, layerY);
       // e.currentTarget.classList.add(styles.drag_start);
       e.currentTarget.setAttribute("layerX", String(layerX));
       e.currentTarget.setAttribute("layerY", String(layerY));
-      e.currentTarget.style.setProperty("transform", "translate(0, 0)");
+      // e.currentTarget.style.setProperty("transform", "translate(0, 0)");
     });
     memo.addEventListener("dragend", (e) => {
-      const { clientX, clientY } = e;
+      const {
+        clientX,
+        clientY,
+        target: { clientWidth, clientHeight },
+      } = e;
       // const { clientWidth, clientHeight } = e.currentTarget;
       const { scrollTop, scrollLeft } = e.target.parentElement;
       const { top, left } = e.target.parentElement.getBoundingClientRect();
@@ -416,24 +543,41 @@ class ImageMemo extends ImageMemoProxy {
     });
     memo.addEventListener("dragover", (e) => e.preventDefault());
     memo.addEventListener("click", (e) => {
-      this._memos.forEach((memo) => memo.classList.remove("focus"));
+      console.log("click ", e, memo.id);
+      this.#memos.forEach((memo) => memo.classList.remove("focus"));
 
       memo.classList.add("focus");
-      this._focus = memo.id;
+      this.#focus = memo.id;
     });
     const textArea = document.createElement("div");
     textArea.contentEditable = true;
     textArea.innerHTML = text;
+    textArea.addEventListener("click", (e) => {
+      e.stopPropagation();
+      console.log("click textArea");
+      memo.classList.add("focus");
+      this.#focus = memo.id;
+    });
+
+    textArea.addEventListener("keydown", (e) => {
+      console.log(e);
+      if (e.ctrlKey && e.key === "Enter") {
+        textArea.blur();
+        memo.classList.remove("focus");
+        this.#focus = null;
+      }
+    });
+
     memo.appendChild(textArea);
     // memo.contentEditable = true;
 
-    this._memos.push(memo);
-    this._ids.push(memoId);
+    this.#memos.push(memo);
+    this.#ids.push(memoId);
     return memo;
   }
 
   setData(data) {
-    this._data = data;
+    this.#data = data;
   }
 
   add(data) {
@@ -444,7 +588,7 @@ class ImageMemo extends ImageMemoProxy {
         x: m.left,
         y: m.top,
       });
-      const canvasEl = document.getElementById(this._rootId + "_canvas");
+      const canvasEl = document.getElementById(this.#rootId + "_canvas");
       canvasEl.appendChild(memo);
     });
 
@@ -471,13 +615,13 @@ class ImageMemo extends ImageMemoProxy {
       path.dataset.endX = endX;
       path.dataset.endY = endY;
 
-      const canvasEl = document.getElementById(this._rootId + "_paint");
+      const canvasEl = document.getElementById(this.#rootId + "_paint");
       canvasEl.appendChild(path);
     });
   }
 
   // set #rootId(a) {
-  //   this._rootId = a;
+  //   this.#rootId = a;
   // }
 
   // get #rootId() {
